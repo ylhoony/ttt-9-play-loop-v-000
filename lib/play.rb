60fb8cae94bb76e5e449f9d173bab6159a56e2cd
Hoon Lee <yl.hoony@gmail.com>
2017-10-25 04:19:23 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -18,7 +18,7 @@ end
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
-
+ 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
